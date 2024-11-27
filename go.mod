1c736554ed23aee60c21630e058d3348bee2e4c9
mateuscavalcant <mateuscavalcant7@gmail.com>
2024-11-27 08:03:17 -0300

    Add model

---
+++ go.mod
@@ -0,0 +1,3 @@
+module social-network-algorithm
+
+go 1.22.2
---
+++ internal/model/model.go
@@ -0,0 +1,18 @@
+package model
+
+type User struct {
+	id       int
+	username string
+}
+
+type AdjacencyNode struct {
+	user *User
+	next *AdjacencyNode
+}
+
+type Graph struct {
+	users        **User
+	adjList      **AdjacencyNode
+	numUsers     int
+	visitedUsers *int
+}
